1ea7924081fc2eec3f81399a0af8e2fbdb0b5f9e
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-06 18:48:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let multiply;
+let num1=31;
+let num2=2;
+multiply =num1*num2
+
+
+ 
+let random;
+random=Math.floor(Math.random(0,99)*10)
+
+let mod;
+let num3=169;
+let num4=5;
+mod=num3%num4;
+
+let max =Math.max(15,16,19,20)
